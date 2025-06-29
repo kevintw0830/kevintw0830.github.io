1f5bfc82cb9c33acb43e150e30d1363fd49aae8b
kevin <kevin@simbo.com.tw>
2025-06-29 12:10:39 +0800

    Deploy FM1 - 2025-06-29 12:10:39

--- FM1/flutter_service_worker.js
+++ FM1/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "41ba11ffc2b9f3388137276c45500aa3",
+const RESOURCES = {"flutter_bootstrap.js": "cf165142dfa89e3633429415deded4c6",
 "version.json": "93c62f4a6a67efbde212a886db9108a9",
 "index.html": "bd705ba9554bded847445d09d81a770e",
 "/": "bd705ba9554bded847445d09d81a770e",
-"main.dart.js": "e0239b5bdb4cc38bfc0ffab677400918",
+"main.dart.js": "58f1b5fa96989bb6d56c8b83d90bb0e6",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
