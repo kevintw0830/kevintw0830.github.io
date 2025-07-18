3cf295e294a924e1c174e5cc214cbcb1ba18c9c3
kevin <kevin@simbo.com.tw>
2025-07-18 22:35:35 +0800

    Deploy NFM1 - 2025-07-18 22:35:35

--- NFM1/flutter_service_worker.js
+++ NFM1/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "373b947afa35e38ae1742f0e01c45806",
-"version.json": "15dfd92f1aee8c146166fdc065798cef",
+const RESOURCES = {"flutter_bootstrap.js": "d891fc3f9f15da7770eadab9fb4b6a9d",
+"version.json": "1b13088fe5075df8e3b9e58bb7391704",
 "index.html": "adc1b022abc45c2360e6ceb4e68e1673",
 "/": "adc1b022abc45c2360e6ceb4e68e1673",
-"main.dart.js": "e76bd62f95a3c203bbebe44695cfa878",
+"main.dart.js": "4f59250ab3eeb4260ffb8aeec6085f12",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "373b947afa35e38ae1742f0e01c45806",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "d4fde5ca449d0e7754b525d00dacbbfd",
-"assets/fonts/MaterialIcons-Regular.otf": "34964e32bc79c43a5eaa220180785fe3",
+"assets/fonts/MaterialIcons-Regular.otf": "f510bc43eca9211fe83af986ab94983e",
 "assets/assets/locales/zh_CN.arb": "d2b5c2f5e61c9a2a60b1f7683a6a1333",
 "assets/assets/locales/zh.arb": "4b8071ba591b3b3fbc8cae3e52e28a11",
 "assets/assets/locales/en.arb": "5f57d9f8e2749845f09ead9298753bac",
