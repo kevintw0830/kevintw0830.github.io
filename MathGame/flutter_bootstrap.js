f3a3f0c42adaf469917e17ba09ed9ef1aa7b5973
kevin <kevin@simbo.com.tw>
2025-11-09 13:06:49 +0800

    Deploy Math Game - 2025-11-09 13:06:49

--- MathGame/flutter_bootstrap.js
+++ MathGame/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1610274259"
+    serviceWorkerVersion: "1290577446"
   }
 });
